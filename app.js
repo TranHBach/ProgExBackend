e87b4c48260eaaab20023c65bcccfb632bcbfaac
TranHBach <ragoza0932447933@gmail.com>
2024-06-28 03:02:08 +0700

    Change CORS

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.use(fileUpload());
 app.use(express.static("public"));
 
 app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  res.append("Access-Control-Allow-Origin", ["https://cooking123.netlify.app"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
