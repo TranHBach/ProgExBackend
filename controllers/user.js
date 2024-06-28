2dedab38624c6dac3d4ee856b432c3f4f5d5e940
TranHBach <ragoza0932447933@gmail.com>
2024-06-28 13:53:32 +0700

    test

--- controllers/user.js
+++ controllers/user.js
@@ -129,11 +129,10 @@ exports.login = async (req, res, next) => {
 exports.logout = (req, res, next) => {
   req.session.destroy(() => {
     res.clearCookie("jwt");
-    return res.end();
+    return res.status(200).json({ message: "Logged out" });
   })
   // res.cookie("jwt", "", {maxAge: 0})
   // res.end();
-  // return res.status(200).json({ message: "Logged out" });
 };
 
 // Tested but may need to change later to make more sense
